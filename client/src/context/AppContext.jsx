e00b483c4d952ca768d99cdc52cbd2df91fe409b
heet1808 <hitpatel488@gmail.com>
2025-02-13 13:31:43 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -13,7 +13,7 @@ const AppContextProvider = (props) => {
 
     const [credit, setCredit] = useState(false)
 
-    const backendUrl = https://imagify-flame-pi.vercel.app
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
     const navigate = useNavigate()
 
     const loadCreditsData = async () => {
